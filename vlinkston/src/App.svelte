2858960ef57de7c1de63fb60d353ed9537479d88
Richard Tammar <richardtammar@gmail.com>
2021-10-25 13:28:45 -0700

    Trivial cleanup

--- vlinkston/src/App.svelte
+++ vlinkston/src/App.svelte
@@ -105,7 +105,6 @@
 
 	function reFilter(event) {
 		console.log(filter);
-		/* Step 1 - Find text links that match */
 		let activeLinks = 0;
 		let filteredBookmarks = $bookmarks;
 
@@ -221,20 +220,6 @@
 	li.selectedTab {
 		color: #fff;
 	}
-	/*
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}*/
 
 	@media (max-width: 640px) {
 		#addBox {
